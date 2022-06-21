486a1ef7b20ca37d2d64fb94cb635d6645058a3d
kaiquefl22@hotmail.com <kaiquefl22@hotmail.com>
2022-06-21 18:16:12 -0300

    adição do button

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Button.css"
+const Button = ({children, onClick}) => {
+	return(
+			<div>
+				<button onClick={onClick} className="button">{children}</button>
+			</div>
+		)
+}
+export default Button;
